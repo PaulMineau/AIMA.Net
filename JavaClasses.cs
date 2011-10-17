b0f5b0f431693be804512711d3e537fa40a6e9da
Paul Mineau <paul.mineau@gmail.com>
2011-10-16 20:12:28 -0700

    initial project version

---
+++ JavaClasses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIMA
+{
+    public class Set<T> : HashSet<T>
+    {
+        public Set()
+        {
+        }
+
+        public Set(IEnumerable<T> initList)
+            : base(initList)
+        {
+
+        }
+        
+
+    }
+
+    public class LinkedHashSet<T> : HashSet<T>
+    {
+
+    }
+
+    public class LinkedHashMap<T1, T2> : Map<T1, T2>
+    {
+
+    }
+
+    public class Map<T1, T2> : Dictionary<T1, T2>
+    {
+    }
+}
